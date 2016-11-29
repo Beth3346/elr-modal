39ed298ed3340d20e2e3cfcd876c93aebb01bae9
Elizabeth Rogers <beth3346@gmail.com>
2016-11-29 16:22:25 -0600

    use es6 syntax

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
 # ELR Modal
 
-[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
-
-A jQuery component
-
-### Dependencies:
-
-+ jQuery
-
-### Features:
-
-### Config:
-
-### Example Usage:
-
-#### Sample html
-
 ###License
 
 ELR-Modal is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
--- src/elr-modal.js
+++ src/elr-modal.js
@@ -1,79 +1,84 @@
-(function($) {
-    window.elrModal = function(params) {
-        var self = {};
-        var spec = params || {};
-        var modalClass = spec.modalClass || 'elr-modal';
-        var lightboxClass = spec.lightboxClass || 'elr-blackout';
-        var closeClass = spec.closeClass || 'elr-modal-close';
-        var buttonClass = spec.buttonClass || 'elr-modal-open';
-        var speed = spec.speed || 300;
-        var $modals = $('.' + modalClass);
+import elrUtlities from 'elr-utilities';
+const $ = require('jquery');
 
-        var createLightbox = function(speed, $modal) {
-            var $close = $('<button></button>', {
-                    'class': 'close',
-                    text: 'x'
-            });
-                
-            var $lightbox = $('<div></div>', {
-                    'class': lightboxClass
-            });
+let elr = elrUtlities();
 
-            $lightbox.hide().appendTo('body').fadeIn(speed, function() {
-                $close.appendTo($lightbox);
-                $modal.appendTo($lightbox).show();
-            });
-        };
+const elrModal = function({
+    modalClass = 'elr-modal',
+    lightboxClass = 'elr-blackout',
+    closeClass = 'elr-modal-close',
+    buttonClass = 'elr-modal-open',
+    speed = 300
+} = {}) {
+    const self = {};
+    const $modals = $(`.${modalClass}`);
 
-        var showModal = function(speed) {
-            var modalId = $(this).data('modal');
-            var $modal = $('#' + modalId);
+    const createLightbox = function(speed, $modal) {
+        const $close = elr.createElement('button', {
+            class: 'close',
+            text: 'x'
+        });
 
-            createLightbox(speed, $modal);
-        };
+        const $lightbox = elr.createElement('div', {
+            class: lightboxClass
+        });
 
-        var hideModal = function(speed) {
-            var $lightbox = $('.' + lightboxClass);
-            var $modal = $lightbox.find('.' + modalClass);
-
-            $lightbox.fadeOut(speed, function() {
-                $modal.hide().appendTo('body');
-                $(this).remove();
-            });
-        }; 
-
-        if ( $modals.length ) {
-            var $body = $('body');
-            
-            $modals.hide().appendTo('body');
-
-            $body.on('click', '.' + buttonClass, function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                showModal.call(this, speed);
-            });
-
-            $('.' + closeClass).on('click', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                hideModal();
-            });
-
-            $body.on('click', '.' + lightboxClass + ' .close', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                hideModal();
-            });
-
-            $body.on('click', function() {
-                hideModal();
-            });
-
-            $modals.on('click', function(e) {
-                e.stopPropagation();
-            });
-        }
-
-        return self;
+        $lightbox.hide().appendTo('body').fadeIn(speed, function() {
+            $close.appendTo($lightbox);
+            $modal.appendTo($lightbox).css({'display': 'flex'});
+        });
     };
-})(jQuery);
+
+    const showModal = function(speed) {
+        const modalId = $(this).data('modal');
+        const $modal = $(`#${modalId}`);
+
+        createLightbox(speed, $modal);
+    };
+
+    const hideModal = function(speed) {
+        const $lightbox = $(`.${lightboxClass}`);
+        const $modal = $lightbox.find(`.${modalClass}`);
+
+        $lightbox.fadeOut(speed, function() {
+            $modal.hide().appendTo('body');
+            $(this).remove();
+        });
+    };
+
+    if ($modals.length) {
+        const $body = $('body');
+
+        $modals.hide().appendTo('body');
+
+        $body.on('click', `.${buttonClass}`, function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            showModal.call(this, speed);
+        });
+
+        $(`.${closeClass}`).on('click', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            hideModal();
+        });
+
+        $body.on('click', `.${lightboxClass} .close`, function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            hideModal();
+        });
+
+        $body.on('click', function() {
+            hideModal();
+        });
+
+        $modals.on('click', function(e) {
+            e.stopPropagation();
+        });
+    }
+
+    return self;
+};
+
+export default elrModal;
